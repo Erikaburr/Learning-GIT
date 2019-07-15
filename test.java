aece282c9f9685f585fb71036bbde535486521a1
Erik Burr <rkburr@gmail.com>
2019-07-14 21:48:34 -0400

    update to test to use junit tests.

--- test.java
+++ test.java
@@ -1,7 +1,15 @@
 public class test{
 
+
+ public static double add(double a, double b){
+return a + b;
+}
+
+
 public static void main(String[] args){
-	System.out.println("test");
+System.out.println(add(3, 3));	
+
+System.out.println("test");
 	System.out.println("Added a newBranch");
 
 }
