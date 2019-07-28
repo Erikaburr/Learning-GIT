695f176287b8fd30d2de898c787ee96c83839dd7
Erik A Burr <42555634+Erikaburr@users.noreply.github.com>
2019-07-28 17:30:01 -0400

    Update test1.java

--- test1.java
+++ test1.java
@@ -9,7 +9,7 @@ return a + b;
 public static void main(String[] args){
 System.out.println(add(3, 3));  
 
-System.out.println("test");
+System.out.println("tests");
     System.out.println("Added a newBranch");
 
 }
