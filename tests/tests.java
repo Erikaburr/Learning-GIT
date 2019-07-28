59af0e4409e154b27a3b4a4251453e09685fe2fb
Erik A Burr <42555634+Erikaburr@users.noreply.github.com>
2019-07-28 17:56:31 -0400

    Update tests.java

--- tests/tests.java
+++ tests/tests.java
@@ -1,1 +1,13 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+class tests {
+
+       @Test
+       public void testMoveDown() {
+            test t = new test();
+            t.add(5,1);
+            assertEquals(6, 6);      
+     }
+}
 
