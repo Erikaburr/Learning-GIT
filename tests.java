a2ed1704bf2c215beff31200cfef0940d890a5ee
Erik Burr <rkburr@gmail.com>
2019-07-28 17:48:02 -0400

    add

---
+++ tests.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+class tests {
+
+       @Test
+       public void testMoveDown() {
+            test t = new test();
+            t.add(5,1);
+            assertEquals(6, 6);      
+       }
+
+}
+
