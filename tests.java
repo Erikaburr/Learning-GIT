022463bbc01f2ce12ac54580295de94a9148fd9b
Erik A Burr <42555634+Erikaburr@users.noreply.github.com>
2019-07-28 16:13:58 -0400

    Create tests.java

---
+++ tests.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import solution.clas;
+
+class unitTest {
+
+       @Test
+       public void testMoveDown() {
+            clas elevator = new clas();
+            elevator.add(5,1);
+            assertEquals(6, 6);      
+       }
+       }
